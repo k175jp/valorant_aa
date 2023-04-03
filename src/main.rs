027543a8a7aeca8c20420cea6704ac36ad74dcac
k175jp <tai.sp.tai@gmail.com>
2023-04-04 00:51:30 +0900

    [feat] create model

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod aa_str;
+
+use aa_str::EXCLAMATION;
+use aa_str::PERIOD;
+use aa_str::SPACE;
+use aa_str::AA;
+
+fn main() {
+
+}
+
+fn create_aa() {
+
+}
+
+fn get_aa() {
+
+}
+
+fn q_parse() {
+
+}
+
+fn extract() {
+
+}
+
+fn parse() {
+
+}
